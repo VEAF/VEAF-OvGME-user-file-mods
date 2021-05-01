3c7ed30112e356ba9b08750988e6965ed7721f94
David Pierron <dpierron@gmail.com>
2021-05-01 15:06:14 +0200

    new "Sounds - F-16C - ECHO19 Viper Audio Overhaul"

---
+++ xSounds_-_F-16C_-_ECHO19_Viper_Audio_Overhaul/README.txt
@@ -0,0 +1,12 @@
+New engine sounds for the F-16C Viper by ECHO19
+
+Nouveaux sons de moteur pour le F-16C Viper par ECHO19
+
+---------------------------------------
+-- Tested with DCS World  2.7.0.5118 --
+---------------------------------------
+
+By Echo 19 (https://www.digitalcombatsimulator.com/fr/files/3316290/)
+
+Zip.
+http://www.veaf.org
---
+++ xSounds_-_F-16C_-_ECHO19_Viper_Audio_Overhaul/Sounds - F-16C - ECHO19 Viper Audio Overhaul/Mods/Tech/ECHO19_F16/entry.lua
@@ -0,0 +1,20 @@
+declare_plugin("Echo19 F-16C Audio Overhaul",
+{
+	installed 	 = true, -- if false that will be place holder , or advertising
+	dirName	  	 = current_mod_path,
+	displayName   = "F-16C (Viper) Audio Overhaul",
+	shortName	  = "E19 F-16C",
+	image     	 = "e19_viper.png",
+	version		  = "1.1.0",
+	developerName = "Echo 19 Audio",
+	info		  = "Echo 19 Audio Proudly Presents: F-16C Viper Audio Overhaul. This mod provides high quality audio for the DCS: F-16C Viper and aims to help enhance the fantastic visuals, with now even better audio. Please report any issues on our official Discord server! Thanks for playing!",
+	state		 = "installed",
+	Skins  =
+	{
+		{
+			name   = "Echo 19 Audio F-16C",
+			dir    = "Media"
+		},
+	},
+})
+plugin_done()
